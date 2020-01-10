70d185b02fb1cafb3fd5a293c56ff23bf5032bbd
Jean van Niekerk <jean.jacque@rocketmail.com>
2020-01-10 09:25:02 +0200

    Finishing up redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import AppNavbar from '../src/components/AppNavbar';
 import ShoppingList from '../src/components/ShoppingList';
+import ItemModal from '../src/components/itemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <AppNavbar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
